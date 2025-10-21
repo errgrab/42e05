66799b3d5a1a998ac0da93507deaad5b93b1dc06
ErikG <eriklustosa@gmail.com>
2025-10-21 13:39:10 +0100

    bsq complete!

--- rendu/bsq/bsq.c
+++ rendu/bsq/bsq.c
@@ -4,32 +4,112 @@
 int w, h;
 char e, o, f;
 
-void scpy(char *dest, char *src, size_t n) {
-	while (n--) *dest++ = *src++;
+char **input = NULL;
+int **map = NULL;
+
+int idx(int x, int y, int w) { return y * w + x; }
+int min3(int a, int b, int c) { return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c); }
+
+void free_input(char **input) {
+	for (int y = 0; y < h; y++) {
+		if (input[y]) free(input[y]);
+	}
+	free(input);
 }
 
-char *gmap(FILE *file) {
-	if (fscanf(file, "%d%c%c%c%c", &h, &e, &o, &f) != 5)
-		return NULL;
-	char *buffer;
-	int n;
-	w = getline(&buffer, &n, file) + 1;
-	if (!buffer) return NULL;
-	char *r = calloc(w * h, sizeof(char));
-	scpy(r, buffer, w);
-	int total = w;
-	int v;
-	while (v = getline(&buffer, &n, file)) {
-		total += v;
+void free_map(int **map) {
+	for (int y = 0; y < h; y++) {
+		if (map[y]) free(map[y]);
 	}
-	free(buffer);
-	return r;
+	free(map);
+}
+
+void get_map(FILE *file) {
+
+	char newline;
+	if (fscanf(file, "%d%c%c%c%c", &h, &e, &o, &f, &newline) != 5 || newline != '\n') return;
+	if (e == o || e == f || f == o || h <= 0) return;
+
+	map = calloc(h, sizeof(int*));
+	if (!map) return;
+
+	input = calloc(h, sizeof(char*));
+	if (!input) return;
+
+	char *buffer = NULL;
+	size_t n = 0;
+	w = getline(&buffer, &n, file) - 1;
+	if (!buffer) return;
+	input[0] = buffer;
+
+	int *line = calloc(w, sizeof(int));
+	if (!line) return;
+
+	for (int x = 0; x < w; x++) {
+		line[x] = 1;
+		if (buffer[x] == o) line[x] = 0;
+	}
+	map[0] = line;
+
+	for (int y = 1; y < h; y++) {
+		buffer = NULL;
+		n = 0;
+		getline(&buffer, &n, file);
+		if (!buffer) return;
+		input[y] = buffer;
+
+		line = calloc(w, sizeof(int));
+		if (!line) return;
+
+		map[y] = line;
+		for (int x = 0; x < w; x++) {
+			if (x == 0)
+				map[y][x] = 1;
+			else if (input[y][x] == o)
+				map[y][x] = 0;
+			else
+				map[y][x] = 1 + min3(map[y - 1][x], map[y][x - 1], map[y - 1][x - 1]);
+		}
+	}
 }
 
 int main(int ac, char **av) {
 	FILE *file;
+
 	if (ac < 2) file = stdin;
 	else file = fopen(av[1], "r");
-	char *map = gmap(file);
+
+	get_map(file);
+
+	if (!map || !input) {
+		fprintf(stderr, "Error: invalid map\n");
+		if (!map) free_map(map);
+		if (!input) free_input(input);
+		return 1;
+	}
+
+	int mx = 0, my = 0, mm = 0;
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			if (map[y][x] > mm) {
+				mm = map[y][x];
+				mx = x; my = y;
+			}
+		}
+	}
+	for (int y = 0; y < h; y++) {
+		for (int x = 0; x < w; x++) {
+			if (x > mx - mm && x <= mx && y > my - mm && y <= my) {
+				printf("%c", f);
+			} else {
+				printf("%c", input[y][x]);
+			}
+		}
+		printf("\n");
+	}
+
 	fclose(file);
+	free_map(map);
+	free_input(input);
+	return 0;
 }
